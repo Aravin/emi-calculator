669deec80d2328780224789f8657ff6c1e2a3917
Aravin <aravin.it@gmail.com>
2019-12-29 11:34:38 +0530

    Removed unused code

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -9,27 +9,7 @@ export class StorageService {
 
   constructor(private storage: Storage, private router: Router) { }
 
-  // getLocation() {
-
-  //   const promise = this.storage.get('location')
-  //     .then((val) => {
-  //       return val;
-  //     })
-  //     .catch((err: any) => {
-  //       return err;
-  //     });
-
-  //   const location = Promise.resolve(promise);
-
-  //   if (!location) {
-  //     this.router.navigate(['/settings']);
-  //   }
-
-  //   return location;
-  // }
-
   setCurrency(location: any): void {
-    console.log(location)
     this.storage.set('symbol', location.symbol_native);
     this.storage.set('code', location.code);
   }
